0595e626f664d9e6afe89745926123556b7ed700
Flaviano <flaviano.junior@enerwatt.com.br>
2020-04-15 12:27:12 -0300

    Adicionado Register

--- ConectaEmpregos/Controllers/AccountController.cs
+++ ConectaEmpregos/Controllers/AccountController.cs
@@ -13,5 +13,10 @@ namespace ConectaEmpregos.Controllers
         {
             return View();
         }
+
+        public ActionResult Register()
+        {
+            return View();
+        }
     }
 }
